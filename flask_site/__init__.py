0b2841d79301249687bd8ab10367950611b7a3ab
Peter Haas <peter@peterhaas.me>
2021-02-21 06:46:02 -0500

    moved code from run.py to flask_site init.py

--- flask_site/__init__.py
+++ flask_site/__init__.py
@@ -1,3 +1,10 @@
+# /flask_site/__init__.py
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+app.debug=True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
--- run.py
+++ run.py
@@ -1,10 +1,5 @@
-# app.py
+# run.py
 from flask_site import app
-from flask import render_template
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
